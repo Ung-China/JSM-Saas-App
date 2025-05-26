7ea11dcb6751310cdcf6a46a519a0126128bd32b
Ing-China <peschina6677@gmail.com>
2025-05-26 16:54:14 +0700

    [✨] : Implement Home Page

--- components/CompanionList.tsx
+++ components/CompanionList.tsx
@@ -22,27 +22,62 @@ const CompanionList = ({
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="text-lg w-2/3">Lession</TableHead>
+            <TableHead className="text-lg w-2/3">Lessons</TableHead>
             <TableHead className="text-lg">Subject</TableHead>
             <TableHead className="text-lg text-right">Duration</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {companions?.map(({ id, subject, name, topic, duration }) => (
+          {companions?.map(({ id, subject, name, topic, duration, color }) => (
             <TableRow key={id}>
               <TableCell>
                 <Link href={`/companions/${id}`}>
                   <div className="flex items-center gap-2">
-                    <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden">
+                    <div
+                      className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
+                      style={{ backgroundColor: color }}
+                    >
                       <Image
                         src={`/icons/${subject}.svg`}
-                        alt="subject"
+                        alt={subject}
                         width={35}
                         height={35}
                       />
                     </div>
+                    <div className="flex flex-col gap-2">
+                      <p className="font-bold text-2xl">{name}</p>
+                      <p className="text-lg">{topic}</p>
+                    </div>
                   </div>
                 </Link>
+              </TableCell>
+              <TableCell>
+                <div className="subject-badge w-fit">{subject}</div>
+                <div
+                  className="flex items-center justify-center rounded-lg w-fit p-2 md:hidden"
+                  style={{ backgroundColor: color }}
+                >
+                  <Image
+                    src={`/icons/${subject}.svg`}
+                    alt={subject}
+                    width={18}
+                    height={18}
+                  />
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="flex items-center gap-2 w-full justify-end">
+                  <p className="text-2xl">
+                    {duration} <span className="max-md:hidden">mins</span>
+                  </p>
+                  <Image
+                    src="/icons/clock.svg"
+                    alt="minutes"
+                    width={14}
+                    height={14}
+                    className="md:hidden"
+                  />
+                </div>
               </TableCell>
             </TableRow>
           ))}
